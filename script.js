19e7c65b3a7391aab5703cb77de1bc2328f738bd
Beata Markunas <beata.markunas@gmail.com>
2018-11-12 21:42:10 +0100

    Improve event.

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const header = document.querySelector("h1");
 
 document.body.addEventListener("mousemove", (event => {
     header.textContent = `${event.clientX}, ${event.clientY}`;
-    document.body.style.backgroundColor = `rgb(${event.clientX}, ${event.clientY}, 100)`
+    document.body.style.backgroundColor = `rgb(${event.clientX/3}, ${event.clientY/2}, 100)`
 }))
