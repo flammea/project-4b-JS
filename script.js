799bda9535b8dabad4b2d52fb90215538d95dce7
Beata Markunas <beata.markunas@gmail.com>
2018-11-13 22:35:01 +0100

    Create new variables.

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const header = document.querySelector("h1");
 
 document.body.addEventListener("mousemove", (event => {
-    header.textContent = `${event.clientX}, ${event.clientY}`;
-    document.body.style.backgroundColor = `rgb(${event.clientX/3}, ${event.clientY/2}, 100)`
+    const x = event.clientX + 1;
+    const y = event.clientY + 1;
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    header.textContent = x + ", " + y;
+
+    const red = x / width * 100;
+    const green = y / height * 100;
+    const blue = (x / width * 100) + (y / height * 100);
+
+    document.body.style.backgroundColor = `rgb(${red}%, ${green}%, ${blue}%)`
 }))
